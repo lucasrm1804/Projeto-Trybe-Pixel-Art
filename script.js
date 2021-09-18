b817bb9c7d5cb245dc4bb3dea0be1d2107eb468d
Lucas Monteiro <lucasribeirom@hotmail.com>
2021-09-18 17:22:32 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // function creatPixel() {
 //   let board = document.getElementById('pixel-board');
 //   let boardSize = 5;
@@ -19,20 +18,146 @@
 // creatPixel();
 //requisito 7 - Função para remover e add a class selected
 //primeiro capturo todos os elementos da paleta
-const cores = document.querySelectorAll(".color")
 
-function selected(){
+const cores = document.querySelectorAll('.color');
+
+function selected() {
   // aqui eu add um excutador de click para remover o select e add o selected conforme o click
   for (let index = 0; index < cores.length; index += 1) {
-    cores[index].addEventListener('click',function () {
+    cores[index].addEventListener('click', function () {
       // removo a class
-      for (let index = 0; index < cores.length; index += 1){
-      cores[index].classList.remove('selected') 
-    }
-    // add a classe
-    cores[index].classList.add('selected')
-   })
+      for (let index = 0; index < cores.length; index += 1) {
+        cores[index].classList.remove('selected');
+      }
+      // add a classe
+      cores[index].classList.add('selected')
+    });
   }
 }
 
 selected();
+
+// requisito 8 - função para pintar os pixels
+//primeiro capturo todos os elementos do board
+
+const black = document.querySelector('#black')
+window.onload = function () {
+  
+
+  const quadradinho = document.querySelectorAll('.pixel');
+  let corSelecionada = document.querySelector('.selected');
+  console.log(corSelecionada)
+  const teste = window.getComputedStyle(corSelecionada).backgroundColor;
+  console.log(teste)
+
+
+    // criar uma função para acessar os pixels e colorir os mesmos.
+
+    function pintarPixel() {
+      for (let i = 0; i < quadradinho.length; i++) {
+        
+        quadradinho[i].addEventListener('click', function (event) {
+          event.target.style.backgroundColor = teste
+        });
+        
+      }
+    }
+
+  pintarPixel()
+  }
+
+black.addEventListener('click', function () {
+const quadradinho = document.querySelectorAll('.pixel');
+let corSelecionada = document.querySelector('.selected');
+console.log(corSelecionada)
+const teste = window.getComputedStyle(corSelecionada).backgroundColor;
+console.log(teste)
+
+
+  // criar uma função para acessar os pixels e colorir os mesmos.
+
+  function pintarPixel() {
+    for (let i = 0; i < quadradinho.length; i++) {
+      
+      quadradinho[i].addEventListener('click', function (event) {
+        event.target.style.backgroundColor = teste
+      });
+      
+    }
+  }
+
+pintarPixel()
+})
+
+const purple = document.querySelector('#purple')
+purple.addEventListener('click', function () {
+const quadradinho = document.querySelectorAll('.pixel');
+let corSelecionada = document.querySelector('.selected');
+console.log(corSelecionada)
+const teste = window.getComputedStyle(corSelecionada).backgroundColor;
+console.log(teste)
+
+
+  // criar uma função para acessar os pixels e colorir os mesmos.
+
+  function pintarPixel() {
+    for (let i = 0; i < quadradinho.length; i++) {
+      
+      quadradinho[i].addEventListener('click', function (event) {
+        event.target.style.backgroundColor = teste
+      });
+      
+    }
+  }
+
+pintarPixel()
+})
+
+const yellow = document.querySelector('#yellow')
+yellow.addEventListener('click', function () {
+const quadradinho = document.querySelectorAll('.pixel');
+let corSelecionada = document.querySelector('.selected');
+console.log(corSelecionada)
+const teste = window.getComputedStyle(corSelecionada).backgroundColor;
+console.log(teste)
+
+
+  // criar uma função para acessar os pixels e colorir os mesmos.
+
+  function pintarPixel() {
+    for (let i = 0; i < quadradinho.length; i++) {
+      
+      quadradinho[i].addEventListener('click', function (event) {
+        event.target.style.backgroundColor = teste
+      });
+      
+    }
+  }
+
+pintarPixel()
+})
+
+const blue = document.querySelector('#blue')
+blue.addEventListener('click', function () {
+const quadradinho = document.querySelectorAll('.pixel');
+let corSelecionada = document.querySelector('.selected');
+console.log(corSelecionada)
+const teste = window.getComputedStyle(corSelecionada).backgroundColor;
+console.log(teste)
+
+
+  // criar uma função para acessar os pixels e colorir os mesmos.
+
+  function pintarPixel() {
+    for (let i = 0; i < quadradinho.length; i++) {
+      
+      quadradinho[i].addEventListener('click', function (event) {
+        event.target.style.backgroundColor = teste
+      });
+      
+    }
+  }
+
+pintarPixel()
+})
+
